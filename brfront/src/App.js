53ba5e487ce9e0a476e34927d9a45393a695460c
Kevin Chang <kckaiwei@gmail.com>
2020-06-02 16:52:18 -0400

    setting up webpack

--- brfront/src/App.js
+++ brfront/src/App.js
@@ -1,17 +1,22 @@
+import '@babel/polyfill'
 import React, { Fragment, useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
 function App() {
   const [data, setData] = useState({ books: [] });
-  const [query, setQuery] = useState('George Orwell');
+  const [query, setQuery] = useState("George Orwell");
 
   useEffect(async () => {
     // Can't return an async function, but can call one in an effect
     const fetchData = async () => {
-      const result = await axios("/recommendations/?count=1");
+      const result = await axios(
+        "http://127.0.0.1:8000/recommendations/?count=1"
+      );
+      console.log(result)
       setData(result.data);
     };
+
     fetchData();
   }, []);
 
@@ -19,19 +24,21 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Fragment>
-            <input type="text" value={query} onChange={event => setQuery(event.target.value)}>
-
-            </input>
-        <ul>
-          {data.books.map((item) => (
-            <li key={item.title}>
-              <div>
-                <a href={item.url}>{item.title}</a>
-              </div>
-              <div>{item.author}</div>
-            </li>
-          ))}
-        </ul>
+          <input
+            type="text"
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+          ></input>
+          <ul>
+            {data.books.map((item) => (
+              <li key={item.title}>
+                <div>
+                  <a href={item.url}>{item.title}</a>
+                </div>
+                <div>{item.author}</div>
+              </li>
+            ))}
+          </ul>
         </Fragment>
       </header>
     </div>
---
+++ brfront/webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: ["@babel/polyfill", "./src/"],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+        {
+          test: /\.css$/,
+            use: [
+                {
+                  loader: "css-loader"
+                }
+            ]
+        }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
