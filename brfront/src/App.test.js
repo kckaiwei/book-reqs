c2e4b638c2a293eb19991509d94b43ed74d5d379
Kevin Chang <kckaiwei@gmail.com>
2020-06-03 09:41:39 -0400

    refactor to use beforeEach()

--- brfront/src/App.test.js
+++ brfront/src/App.test.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 
 import axios from "axios";
 
@@ -8,7 +8,7 @@ import Adapter from "enzyme-adapter-react-16";
 configure({ adapter: new Adapter() });
 
 import App from "./App.js";
-import {fetchData} from "./utils";
+import { fetchData } from "./utils";
 
 // Need to mock modules
 jest.mock("axios");
@@ -27,10 +27,9 @@ describe("<App/>", () => {
 });
 
 describe("test app hooks", () => {
-
-  beforeEach(() =>{
-    fetchData.mockImplementation(() => {})
-  })
+  beforeEach(() => {
+    fetchData.mockImplementation(() => {});
+  });
 
   const resp = {
     data: [
@@ -43,19 +42,68 @@ describe("test app hooks", () => {
     ],
   };
 
+  const countResp = {
+    data: [
+      {
+        title:
+          "The Blind Watchmaker: Why the Evidence of Evolution Reveals a Universe without Design",
+        author: "Richard Dawkins",
+      },
+      {
+        title: "The Really Hard Problem: Meaning in a Material World",
+        author: "Owen Flanagan",
+      },
+      { title: "The Character of Physical Law", author: "Richard Feynman" },
+      {
+        title: "The Demon-Haunted World: Science as a Candle in the Dark",
+        author: "Carl Sagan",
+      },
+      {
+        title: "The Singularity is Near: When Humans Transcend Biology",
+        author: "Ray Kurzweil",
+      },
+      { title: "Tao Te Ching", author: "Lao Tzu" },
+      { title: "Thinking, Fast and Slow", author: "Daniel Kahneman" },
+      {
+        title: "The History of the Decline and Fall of the Roman Empire",
+        author: "Edward Gibbon",
+      },
+      { title: "Why Evolution Is True", author: "Jerry Coyne" },
+    ],
+  };
+
   it("calls the fetch function", () => {
     const wrapper = mount(<App />);
     expect(fetchData).toHaveBeenCalled();
   });
 
   it("creates a book list", () => {
-    axios.get.mockResolvedValue(resp);
-    fetchData.mockImplementation(function(query, userList, setData){
-      setData(resp)
-    })
+    fetchData.mockImplementation(function (query, userList, setData) {
+      setData(resp);
+    });
 
     const wrapper = mount(<App />);
     expect(wrapper.find("li")).toHaveLength(1);
-    expect(wrapper.find("li").first().find("span").first().text()).toEqual('1984')
+    expect(wrapper.find("li").first().find("span").first().text()).toEqual(
+      "1984"
+    );
+  });
+
+  describe("test multiple book list", () => {
+    beforeEach(() => {
+      fetchData.mockImplementation(function (query, userList, setData) {
+        setData(countResp);
+      });
+    });
+
+    it("clicking selection marks active", () => {});
+
+    it("creates an count list", () => {
+      const wrapper = mount(<App />);
+      expect(wrapper.find("li")).toHaveLength(9);
+      expect(wrapper.find("li").first().find("span").first().text()).toEqual(
+        "The Blind Watchmaker: Why the Evidence of Evolution Reveals a Universe without Design"
+      );
+    });
   });
 });
